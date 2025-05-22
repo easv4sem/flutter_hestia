507dbc942aac1bc1fdefd077c4b2b63279be910c
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-05-22 10:21:52 +0200

    feat: Update EnumSensorType and Sensor model for improved JSON handling and null safety

--- lib/models/enum_sensor_type.dart
+++ lib/models/enum_sensor_type.dart
@@ -1,13 +1,13 @@
 enum EnumSensorType {
-      camera,
-    bme280, // Barometer
-    mq135mq2, // Air Quality sensor
-    soilMoisture, // Soil Moisture sensor
-    unknown; 
+  camera,
+  bme280, // Barometer
+  mq135mq2, // Air Quality sensor
+  soilMoisture, // Soil Moisture sensor
+  unknown;
 
-  static EnumSensorType fromString(String type) {
+  static EnumSensorType fromString(String? type) {
     return EnumSensorType.values.firstWhere(
-      (e) => e.toString().split('.').last == type.toLowerCase(),
+      (e) => e.toString().split('.').last == type?.toLowerCase(),
       orElse: () => EnumSensorType.unknown,
     );
   }
@@ -15,8 +15,6 @@ enum EnumSensorType {
   static EnumSensorType fromNumber(int type) {
     return EnumSensorType.values[type];
   }
-
-
 }
 
 extension EnumSensorTypeExtension on EnumSensorType {
--- lib/models/sensor.dart
+++ lib/models/sensor.dart
@@ -1,39 +1,47 @@
 import 'enum_sensor_type.dart';
+
 class Sensor {
-    final String uniqueIdentifier;
-    final String? displayName;
-    final String? serialNumber;
-    final DateTime? lastChangeDate;
-    final EnumSensorType? type;
-    
+  final String uniqueIdentifier;
+  final String? displayName;
+  final String? serialNumber;
+  final DateTime? lastChangeDate;
+  final EnumSensorType? type;
 
-    Sensor({
-        required this.uniqueIdentifier,
-        this.displayName,
-        this.serialNumber,
-        this.lastChangeDate,
-        this.type,
-    });
+  Sensor({
+    required this.uniqueIdentifier,
+    this.displayName,
+    this.serialNumber,
+    this.lastChangeDate,
+    this.type,
+  });
 
-    factory Sensor.fromJson(Map<String, dynamic> json) {
-                
-        return Sensor(
-            uniqueIdentifier: json['UniqueIdentifier'] as String,
-            displayName: json['DisplayName'] as String,
-            serialNumber: json['SerialNumber'] as String,
-            lastChangeDate: DateTime.parse(json['LastChangeDate'] as String),
-            type: EnumSensorType.fromString(json['Type'] as String),
-        );
-    }
+  factory Sensor.fromJson(Map<String, dynamic> json) {
+    final typeString =
+        json['Type'] as String? ??
+        EnumSensorType.unknown.toString().split('.').last;
 
-    Map<String, dynamic> toJson() {
-        return {
-            'UniqueIdentifier': uniqueIdentifier,
-            'DisplayName': displayName,
-            'SerialNumber': serialNumber,
-            'LastChangeDate': lastChangeDate?.toIso8601String(),
-            'Type': type.toString().split('.').last,
-        };
-    }
+    return Sensor(
+      uniqueIdentifier: json['UniqueIdentifier'] as String,
+      displayName: json['DisplayName'] as String?,
+      serialNumber: json['SerialNumber'] as String?,
+      lastChangeDate:
+          json['LastChangeDate'] != null
+              ? DateTime.parse(json['LastChangeDate'] as String)
+              : null,
+      type: EnumSensorType.values.firstWhere(
+        (e) => e.toString().split('.').last == typeString,
+        orElse: () => EnumSensorType.unknown,
+      ),
+    );
+  }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'UniqueIdentifier': uniqueIdentifier,
+      'DisplayName': displayName,
+      'SerialNumber': serialNumber,
+      'LastChangeDate': lastChangeDate?.toIso8601String(),
+      'Type': type.toString().split('.').last,
+    };
+  }
 }
