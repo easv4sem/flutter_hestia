6c3cc2a14d67c443ac16af8929298277d25187b5
Mads Knudsen <mads@rknudsen.dk>
2025-06-10 08:36:34 +0200

    feat: Add CookieManager and CookieJar dependencies for improved session handling

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter/foundation.dart'; // <- Tilføj dette for kIsWeb
 import 'package:hestia/core/app_constants.dart';
 
 class ApiService {
@@ -6,6 +9,18 @@ class ApiService {
       Dio()
         ..options.headers = {'Content-Type': 'application/json'}
         ..options.baseUrl = AppConstants.apiBaseUrl;
+
+  final CookieJar _cookieJar = CookieJar();
+
+  ApiService() {
+    // Tilføj kun CookieManager hvis vi ikke er på Web
+    if (!kIsWeb) {
+      _dio.interceptors.add(CookieManager(_cookieJar));
+    } else {
+      // Hvis du har brug for at sætte withCredentials på Web:
+      _dio.options.extra = {'withCredentials': true};
+    }
+  }
 
   Future<Response> get(String endpoint) async {
     try {
@@ -27,10 +42,7 @@ class ApiService {
 
   Future<Response> put(String endpoint, Map<String, dynamic> data) async {
     try {
-
       print("PUT request to $endpoint with data: $data");
-
-
       final response = await _dio.put(endpoint, data: data);
       return response;
     } on DioException catch (e) {
